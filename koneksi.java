a679577409c4afbddbdca2834056c3b4d0542c32
anisaratripramudita <90444455+anisaratripramudita@users.noreply.github.com>
2022-04-18 20:51:59 +0700

    modul kasir

---
+++ koneksi.java
@@ -0,0 +1,28 @@
+package projekbesar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    private static Connection koneksi;
+    
+    public static Connection getKoneksi(){
+        if (koneksi==null) {
+            try{
+                String url ="jdbc:mysql://localhost/db_sipbar";
+                String user="root";
+                String pass="";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url,user,pass);
+                System.out.println("koneksi berhasil;");
+            } catch(SQLException e) {
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    } 
+    public static void main(String args[]){
+        getKoneksi();
+    }
+}
