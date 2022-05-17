ebb3bf425e97d0425ef3aa7dce3b2590646450ac
Winaayaa <salsabilawina03@gmail.com>
2022-05-18 04:50:29 +0700

    Laporan Toko

---
+++ LaporanToko/src/koneksi/koneksi.java
@@ -0,0 +1,25 @@
+package koneksi;
+
+import java.sql.*;
+
+public class koneksi {
+    private Connection koneksi;
+    
+    public Connection connect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Berhasil Koneksi.");
+        } catch(ClassNotFoundException ex) {
+            System.out.println("Gagal Koneksi." +ex);
+        }
+        
+        String url = "jdbc:mysql://localhost:3306/toko_buku";
+        try {
+           koneksi =  DriverManager.getConnection(url,"root", "");
+           System.out.println("Berhasil Koneksi Database.");
+        } catch (SQLException ex) {
+            System.out.println("Gagal Koneksi Database." +ex);
+        }
+        return koneksi;
+    }
+}
